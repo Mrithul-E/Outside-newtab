b5902e84211d38467f69ed46495b58c27bd6ca91
Mrithul-E <mrithul100@gmail.com>
2026-08-22 23:59:02 +0530

    Added object for weather code to condition name and emoji

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,47 +3,46 @@ $(function () {
     const searchInput = $("#searchBox")
     const searchBtn = $("#search-btn")
 
-    const weatherCodes = {
-        0: "Clear sky",
+    const weatherInfo = {
+        0: ["Clear sky", "☀️"],
 
-        1: "Mainly clear",
-        2: "Partly cloudy",
-        3: "Overcast",
+        1: ["Mainly clear", "🌤️"],
+        2: ["Partly cloudy", "⛅"],
+        3: ["Overcast", "☁️"],
 
-        45: "Fog",
-        48: "Depositing rime fog",
+        45: ["Fog", "🌫️"],
+        48: ["Depositing rime fog", "🌫️"],
 
-        51: "Light drizzle",
-        53: "Moderate drizzle",
-        55: "Dense drizzle",
+        51: ["Light drizzle", "🌦️"],
+        53: ["Moderate drizzle", "🌦️"],
+        55: ["Dense drizzle", "🌦️"],
 
-        56: "Light freezing drizzle",
-        57: "Dense freezing drizzle",
+        56: ["Light freezing drizzle", "🌧️"],
+        57: ["Dense freezing drizzle", "🌧️"],
 
-        61: "Slight rain",
-        63: "Moderate rain",
-        65: "Heavy rain",
+        61: ["Slight rain", "🌧️"],
+        63: ["Moderate rain", "🌧️"],
+        65: ["Heavy rain", "🌧️"],
 
-        66: "Light freezing rain",
-        67: "Heavy freezing rain",
+        66: ["Light freezing rain", "🌧️"],
+        67: ["Heavy freezing rain", "🌧️"],
 
-        71: "Slight snow fall",
-        73: "Moderate snow fall",
-        75: "Heavy snow fall",
+        71: ["Slight snow fall", "🌨️"],
+        73: ["Moderate snow fall", "🌨️"],
+        75: ["Heavy snow fall", "🌨️"],
 
-        77: "Snow grains",
+        77: ["Snow grains", "🌨️"],
 
-        80: "Slight rain showers",
-        81: "Moderate rain showers",
-        82: "Violent rain showers",
+        80: ["Slight rain showers", "🌦️"],
+        81: ["Moderate rain showers", "🌦️"],
+        82: ["Violent rain showers", "🌧️"],
 
-        85: "Slight snow showers",
-        86: "Heavy snow showers",
+        85: ["Slight snow showers", "🌨️"],
+        86: ["Heavy snow showers", "🌨️"],
 
-        95: "Thunderstorm",
-
-        96: "Thunderstorm with slight hail",
-        99: "Thunderstorm with heavy hail"
+        95: ["Thunderstorm", "⛈️"],
+        96: ["Thunderstorm with slight hail", "⛈️"],
+        99: ["Thunderstorm with heavy hail", "⛈️"]
     };
 
     function getWeather() {
@@ -63,7 +62,7 @@ $(function () {
             );
 
             const data = await response.json();
-            data.current["weather"] = weatherCodes[data.current.weather_code]
+            data.current["weather"] = weatherInfo[data.current.weather_code]
 
             console.log(data.current);
         });
